49491cbf4a6ed6652893cc3e9434e06fc15c7c32
Daniel Vivas <danisanti93@hotmail.com>
2015-09-16 21:38:57 -0500

    Bloques con comentarios y elsif.

--- bloques.rb
+++ bloques.rb
@@ -4,11 +4,14 @@ Se debe utilizar (==) para comparar.
 Cuando se utiliza (=) es para asignar un valor.
 Muchas veces se utiliza (then), pero no es obligatorio su uso.
 =end
+
+puts "Ejecutando linea numero 8."
 if "Daniel" == "Daniel" then
 	puts "Daniel es igual a Daniel"
 end
 
 # Rescatando el valor que comparamos. 
+puts "Ejecutando linea numero 14."
 if "Andres" == "Julio"
 	puts "Andres es igual a Julio"
 else
@@ -17,11 +20,25 @@ end
 
 # PRUEBA!
 # Construye un bloque que me valide el resultado de una suma (2+5).
+puts "Ejecutando linea numero 23."
 if 7 == 2 + 5
 	puts "El resultado es 7"
 end
 
-# Provocando un error.
+=begin # Provocando un error.
+puts "Ejecutando linea numero 8."
 if "Andres" == pepito
 	puts "Andres es igual a Andres"
+=end
+
+# Bloque de dos condiciones
+puts "***************************"
+puts "Ejecutando linea numero 35."
+if "hola" ==  "chao"
+	puts "Hola es igual a chao."
+elsif "chao" == "chao"
+	puts "Chao es igual a chao."
+else
+	puts "No se cumple con ninguna condicion."
 end
+		
