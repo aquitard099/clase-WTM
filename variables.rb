2466d82ac7435f6fa3ff0e303cbfc328afe27bde
Daniel Vivas <danisanti93@hotmail.com>
2015-09-16 19:14:20 -0500

    Repaso de variables y tipos de datos, ademas de puts y print.

---
+++ variables.rb
@@ -0,0 +1,30 @@
+# Define una variable como dato de cadena de texto.
+variable = "Primera variable"
+# Define una variable como un dato tipo float.
+miVariable = 1.67
+# Define una variable com oun dato tipo boolea.
+booleanVar = true
+
+=begin
+Se definen diferentes tipos de variables de la forma correcta.
+Al mismo tiempo las variables las defino como mi nombre y apellidos.
+Es preferible utilizar varNombre a excepcion de las otras dos formas. 	
+=end
+primer_nombre = "Daniel"
+primerApellido = "Vivas"
+apellido2 = "Perez"
+
+# Define una variable como el total de una suma.
+sumaSencilla = 2 + 5
+# Define una variable como el total de la suma de dos variables.
+varYVar = miVariable = sumaSencilla
+# Define una variable con un dato tipo cadena de texto.
+texto = "Final"
+
+# Imprimi algunos datos que tenia como variables.
+puts texto 
+puts "******************"
+print booleanVar
+puts "******************"
+puts miVariable
+
