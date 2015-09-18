f18f33e76b9b91ed2d72b8c57a11aba75ab7da98
Daniel Vivas <danisanti93@hotmail.com>
2015-09-17 19:44:33 -0500

    Ejercicio de errores en un script.

---
+++ ejercicio3.rb
@@ -0,0 +1,16 @@
+Nombre = "Daniel"
+nombre = "daniel"
+EDAD = 21
+ciudad = "Bogota"
+
+# Bogota es igual a bogota
+# Deberia ejecutar el puts.
+if ciudad == "Bogota" 
+	puts "La ciudad es Bogota"
+end
+
+# El arreglo deberia tener 5 elementos 
+arreglo = ["Manzana", "Limon", 3, 2.4, "Pera"]
+
+# Quiero imprimir el ultimo elemento del arreglo.
+puts arreglo[4]
